b3e8335a873ee9b746698c69fdf1548089c72aac
naylE Dev <eliandasilvaoliveira9@proton.me>
2026-08-11 11:02:20 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ use std::io;
 mod caesar;
 mod xor;
 mod hash;
+mod aes;
 mod utils;
 
 use caesar::caesar;
 use xor::xor;
 use hash::hash;
 use utils::para_hex;
+use aes::aes;
+use aes::decifrar_aes;
 
 fn main(){
   
